bc3b94559fb8956738eb28e8a050ef11abc6fb02
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-12-18 20:30:46 -0400

    fix: wrong path to card text file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { join } from 'path';
 import { readFileSync } from 'fs';
 
-const CARD_TEXT_FILE_PATH = join(new URL('..', import.meta.url).pathname, 'card_text');
+const CARD_TEXT_FILE_PATH = join(new URL('card_text', import.meta.url).pathname);
 
 export const cardText = readFileSync(CARD_TEXT_FILE_PATH, 'utf-8');
