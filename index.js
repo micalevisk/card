bac9c82a5c07a15145820b3c2eef7d806289311c
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-12-18 20:24:40 -0400

    fix: `path` is not defined reference error

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import { join } from 'path';
 import { readFileSync } from 'fs';
 
-const CARD_TEXT_FILE_PATH = path.join(new URL('..', import.meta.url).pathname, 'card_text');
+const CARD_TEXT_FILE_PATH = join(new URL('..', import.meta.url).pathname, 'card_text');
 
 export const cardText = readFileSync(CARD_TEXT_FILE_PATH, 'utf-8');
